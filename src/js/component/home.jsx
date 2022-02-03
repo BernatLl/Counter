27bf4d75ddddfa35c91eaaad7922fcd32f4489ba
BernatLl <blloret@hotmail.com>
2022-02-03 15:19:00 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../../styles/index.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [seconds, setSeconds] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setSeconds((seconds) => seconds + 1);
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="counter">
+			<div className="one">{seconds}</div>
 		</div>
 	);
 };
